ef9ee51b236c497eaa777fdb3e23feca97dd37de
Rosa de La Vega <rosa@usto.re>
2024-05-17 11:09:01 -0300

    Criando projeto Sabor-Express

---
+++ sabor-express/teste.py
@@ -0,0 +1,23 @@
+print('''Hello word
+      ''')
+
+# fsymbols]
+
+print('''Python na Escola de Programação da Alura
+      ''')
+
+#Criar variáveis
+nome = 'Rosa'
+idade = 50
+
+#Abordagem mais simples
+#print('Meu nome é', nome, 'e tenho', idade, 'anos.')
+
+#Abordagem do f-string (f'{}')
+print(f'Meu nome é {nome} e tenho {idade} anos.')
+
+#Abordagem do .format()
+#print('Meu nome é {} e tenho {} anos.'.format(nome,idade))
+
+#Abordagem da % (Formação de String Antiga - Python 2)
+#print('Meu nome é %s e tenho %i anos.'%(nome,idade))
